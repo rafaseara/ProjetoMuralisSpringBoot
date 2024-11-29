7e1bbbed605c404ecb214c58591784f0c4f050cf
rafaseara <rafaseara257@gmail.com>
2024-11-29 20:42:49 -0300

    Classe exceptions, não necessária mas praticando-se uma boa prática resolvi separar em uma classe

---
+++ api-restful/src/main/java/com/muralis/api_restful/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.muralis.api_restful.exceptions;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ api-restful/src/main/java/com/muralis/api_restful/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.muralis.api_restful.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequest(BadRequestException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.internalServerError().body("Erro inesperado: " + ex.getMessage());
+    }
+}
