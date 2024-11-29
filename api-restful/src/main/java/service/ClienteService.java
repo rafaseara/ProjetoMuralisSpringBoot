9cd1cc24456c8a678c388f2f22e414c41631a2de
rafaseara <rafaseara257@gmail.com>
2024-11-29 20:25:00 -0300

    Classe service para início das funcionalidades do CRUD

---
+++ api-restful/src/main/java/service/ClienteService.java
@@ -0,0 +1,44 @@
+package service;
+
+import java.util.List;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.muralis.api_restful.entity.Cliente;
+import com.muralis.api_restful.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente findById(Long id) {
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+    }
+
+    public List<Cliente> findByNome(String nome) {
+        return clienteRepository.findByNomeContaining(nome);
+    }
+
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente update(Long id, Cliente cliente) throws BadRequestException {
+        Cliente clienteExistente = findById(id);
+        clienteExistente.setNome(cliente.getNome());
+        clienteExistente.setContato(cliente.getContato());
+        return clienteRepository.save(clienteExistente);
+    }
+
+    public void delete(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
